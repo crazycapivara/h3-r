6beca5db0ce90c52b651eda934a57b59970db5bb
skuethe <crazycapivara@gmail.com>
2019-01-16 19:02:01 +0100

    Add 'geo_to_h3' S3 method for 'data.frame'

--- R/h3_indexing.R
+++ R/h3_indexing.R
@@ -1,18 +1,31 @@
+#' Index geo-coordinates at the resolution into h3 addresses
+#' @param latlng geo coordinates as lat/lng pairs or object of class \code{sf}
+#' @param res resolution; between 0 and 15
+#' @name geo_to_h3
 #' @export
 geo_to_h3 <- function(latlng, res) {
   UseMethod("geo_to_h3")
 }
 
+#' @name geo_to_h3
 #' @export
 geo_to_h3.numeric <- function(latlng, res = 7) {
   matrix(latlng, ncol = 2) %>% rcpp_geo_to_h3(res)
 }
 
+#' @name geo_to_h3
 #' @export
 geo_to_h3.matrix <- function(latlng, res = 7) {
   rcpp_geo_to_h3(latlng, res)
 }
 
+#' @name geo_to_h3
+#' @export
+geo_to_h3.data.frame <- function(latlng, res = 7) {
+  as.matrix(latlng) %>% rcpp_geo_to_h3(res)
+}
+
+#' @name geo_to_h3
 #' @export
 geo_to_h3.sf <- function(latlng, res = 7) {
   sf::st_coordinates(latlng)[, 2:1] %>% rcpp_geo_to_h3(res)
