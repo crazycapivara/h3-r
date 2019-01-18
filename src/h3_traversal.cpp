d658442ca7f48058e0e4451dc8597ce8bafa96fc
skuethe <crazycapivara@gmail.com>
2019-01-18 09:20:59 +0100

    Refactor

--- src/h3_traversal.cpp
+++ src/h3_traversal.cpp
@@ -11,8 +11,15 @@ CharacterVector rcpp_k_ring(String h3s, int radius) {
   int n = maxKringSize(radius);
   H3Index* out = (H3Index*)calloc(n, sizeof(H3Index));
   kRing(h3, radius, out);
-  CharacterVector z(n);
-  for(int i = 0; i < n; ++i) {
+  int counter;
+  for (int i = 0; i < n; ++i) {
+    if (out[i] != 0) {
+      ++counter;
+    }
+  }
+
+  CharacterVector z(counter);
+  for(int i = 0; i < counter; ++i) {
     char h3s[17];
     h3ToString(out[i], h3s, sizeof(h3s));
     z[i] = h3s;
