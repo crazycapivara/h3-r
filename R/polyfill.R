6ad03cfa94e6bae6af72d3c2162e1fb6f77b3ea7
skuethe <crazycapivara@gmail.com>
2019-10-11 11:37:38 +0200

    Support 'sfc' and 'sf' objects in 'polyfill' func

--- R/polyfill.R
+++ R/polyfill.R
@@ -1,5 +1,34 @@
-#' @export
-polyfill <- function(polygon, res = 7) {
+polyfill_ <- function(polygon, res = 7) {
   x <- rcpp_polyfill(polygon, res)
   x[x != ""]
+}
+
+#' Get all hexagons with centers contained in a given polygon.
+#' @param polygon geo-coordinates of polygon as lng/lat pairs (matrix) or object of class \code{sf}
+#'   or \code{sfc}
+#' @inheritParams geo_to_h3
+#' @export
+polyfill <- function(polygon, res = 7) {
+  UseMethod("polyfill", polygon)
+}
+
+#' @name polyfill
+#' @export
+polyfill.matrix <- function(polygon, res = 7) {
+  polyfill_(polygon, res)
+}
+
+#' @name polyfill
+#' @export
+polyfill.sfc <- function(polygon, res = 7) {
+  lnglat <- c("X", "Y")
+  sf::st_coordinates(polygon[1])[, lnglat] %>%
+    polyfill(res)
+}
+
+#' @name polyfill
+#' @export
+polyfill.sf <- function(polygon, res = 7) {
+  sf::st_geometry(polygon) %>%
+    polyfill(res)
 }
--- tests/testthat/test_h3_regions.R
+++ tests/testthat/test_h3_regions.R
@@ -1,15 +1,31 @@
-context("regions")
+context("H3 regions")
+
+read_nc <- function() {
+  sf::st_read(system.file("shape/nc.shp", package = "sf"),  quiet = TRUE) %>%
+    sf::st_transform(4326)
+}
 
 test_that("polyfill matrix", {
   # Prepare
   resolution <- 7
-  nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"),  quiet = TRUE) %>%
-    sf::st_transform(4326)
+  nc <- read_nc()
   lnglat <- c("X", "Y")
   polygon <- sf::st_coordinates(nc[1, ])[, lnglat]
 
   # Act
   h3_indexes <- polyfill(polygon, resolution)
+
+  # Assert
+  expect_length(h3_indexes, 237)
+})
+
+test_that("polyfill sfc/sf", {
+  # Prepare
+  resolution <- 7
+  nc <- read_nc()
+
+  # Act
+  h3_indexes <- polyfill(nc, 7)
 
   # Assert
   expect_length(h3_indexes, 237)
