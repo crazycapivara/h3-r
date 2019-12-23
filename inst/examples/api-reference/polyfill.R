5b7fca85d2780785165ec895fd9f20dd5567b927
skuethe <crazycapivara@gmail.com>
2019-12-23 11:18:20 +0100

    Update example

--- inst/examples/api-reference/polyfill.R
+++ inst/examples/api-reference/polyfill.R
@@ -1,3 +1,3 @@
 nc <- sf::st_read(system.file("shape/nc.shp", package = "sf"),  quiet = TRUE) %>%
   sf::st_transform(4326)
-h3_indexes <- polyfill(nc[1,], res = 7)
+h3_indexes <- polyfill(nc[1, ], res = 7)
